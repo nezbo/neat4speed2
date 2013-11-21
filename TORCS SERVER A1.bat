76aa63cd2317a825e1ff42e85ddbaaaf40a95f7e
EmilDesktop <emilovicmail@gmail.com>
2013-11-21 20:00:40 +0100

    batch server and scenario.xml files, and some results

---
+++ TORCS SERVER A1.bat
@@ -0,0 +1,17 @@
+C:
+cd "C:\Program Files (x86)\torcs"
+
+
+for /l %%x in (1, 1, 10) do wtorcs.exe -T -r SCENARIOS/scenario1.xml
+
+for /l %%x in (1, 1, 10) do wtorcs.exe -T -r SCENARIOS/scenario2.xml
+
+for /l %%x in (1, 1, 10) do wtorcs.exe -T -r SCENARIOS/scenario3.xml
+
+for /l %%x in (1, 1, 10) do wtorcs.exe -T -r SCENARIOS/scenario4.xml
+
+for /l %%x in (1, 1, 10) do wtorcs.exe -T -r SCENARIOS/scenario5.xml
+
+for /l %%x in (1, 1, 10) do wtorcs.exe -T -r SCENARIOS/scenario6.xml
+
+pause
---
+++ TORCS SERVER A2.bat
@@ -0,0 +1,7 @@
+C:
+cd "C:\Program Files (x86)\torcs"
+
+
+for /l %%x in (1, 1, 60) do wtorcs.exe -T -r SCENARIOS/scenario6.xml
+
+pause
